069d2bc04fabd8c6c66687984587caef04cd613e
Adhytia <adit.14.12.99@gmail.com>
2020-04-08 19:39:24 +0700

    Initial commit

---
+++ src/main/java/com/tkap11/spendingrecord/SpendingrecordApplication.java
@@ -0,0 +1,13 @@
+package com.tkap11.spendingrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingrecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingrecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkap11/spendingrecord/SpendingrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkap11.spendingrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingrecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
