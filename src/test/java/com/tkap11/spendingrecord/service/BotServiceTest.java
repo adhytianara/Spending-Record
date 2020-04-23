f6c59bab69e18f2f65f98ecc81d602d17ee2e077
Adhytia <adit.14.12.99@gmail.com>
2020-04-23 17:06:17 +0700

    create test

---
+++ src/test/java/com/tkap11/spendingrecord/service/BotServiceTest.java
@@ -0,0 +1,95 @@
+package com.tkap11.spendingrecord.service;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.UserSource;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BotServiceTest {
+
+    @Mock
+    private BotTemplate botTemplate;
+
+    @InjectMocks
+    private BotService botService;
+
+    @Mock
+    private LineMessagingClient lineMessagingClient;
+
+    @Test
+    void greetingMessageTest() {
+        // mock line bot api client response
+        when(lineMessagingClient.replyMessage(new ReplyMessage(
+                "replyToken", singletonList(null)
+        ))).thenReturn(CompletableFuture.completedFuture(
+                new BotApiResponse("ok", Collections.emptyList())
+        ));
+
+        botService.greetingMessage("replyToken");
+
+        // confirm createFlexMenu is called
+        verify(botTemplate, times(1)).createFlexMenu();
+    }
+
+    @Test
+    void handleMessageEventWhenUserSendMenuMessage() {
+        // if user send "menu" as text message
+        final MessageEvent request = new MessageEvent<>(
+                "replyToken",
+                new UserSource("userId"),
+                new TextMessageContent("id", "menu"),
+                Instant.now()
+        );
+
+        // mock line bot api client response
+        when(lineMessagingClient.replyMessage(new ReplyMessage(
+                "replyToken", singletonList(null)
+        ))).thenReturn(CompletableFuture.completedFuture(
+                new BotApiResponse("ok", Collections.emptyList())
+        ));
+
+        botService.handleMessageEvent(request);
+
+        // confirm createFlexMenu is called
+        verify(botTemplate, times(1)).createFlexMenu();
+    }
+
+    @Test
+    void handleMessageEventWhenUserSendOtherThanMenuMessage() {
+        final MessageEvent request = new MessageEvent<>(
+                "replyToken",
+                new UserSource("userId"),
+                new TextMessageContent("id", "text"),
+                Instant.now()
+        );
+
+        // mock line bot api client response
+        when(lineMessagingClient.replyMessage(new ReplyMessage(
+                "replyToken", singletonList(new TextMessage("Sedang dalam pengembangan"))
+        ))).thenReturn(CompletableFuture.completedFuture(
+                new BotApiResponse("ok", Collections.emptyList())
+        ));
+
+        botService.handleMessageEvent(request);
+
+        // confirm createFlexMenu is called
+        verify(lineMessagingClient).replyMessage(new ReplyMessage(
+                "replyToken", singletonList(new TextMessage("Sedang dalam pengembangan"))
+        ));    }
+}
---
+++ src/test/java/com/tkap11/spendingrecord/service/BotTemplateTest.java
@@ -0,0 +1,16 @@
+package com.tkap11.spendingrecord.service;
+
+import com.linecorp.bot.model.message.FlexMessage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BotTemplateTest {
+    private final BotTemplate botTemplate = new BotTemplate();
+
+    @Test
+    void createFlexMenu() {
+        FlexMessage flexMessage = botTemplate.createFlexMenu();
+        assertTrue(flexMessage instanceof FlexMessage);
+    }
+}
