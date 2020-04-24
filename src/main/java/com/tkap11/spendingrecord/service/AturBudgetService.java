b6f569c9c62486fd99c2b6141358a083d264a088
Ismail Shalih Abdul Kholiq <ismailshalih@gmail.com>
2020-04-25 02:49:05 +0700

    init AturBudgetService

---
+++ src/main/java/com/tkap11/spendingrecord/service/AturBudgetService.java
@@ -0,0 +1,12 @@
+package com.tkap11.spendingrecord.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AturBudgetService {
+
+    //set budget for specific category
+    public void setCategoryBudget(String category, int number) {
+        return;
+    }
+}
