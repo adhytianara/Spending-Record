a49690cb18bb4cba0861a894107fd504083b469d
Adhytia <adit.14.12.99@gmail.com>
2020-04-29 23:25:17 +0700

    [RED] Create new test

---
+++ src/main/java/com/tkap11/spendingrecord/model/User.java
@@ -0,0 +1,33 @@
+package com.tkap11.spendingrecord.model;
+
+public class User {
+    public User(Long id, String userId, String displayName) {
+    }
+
+    public Long getId() {
+        //TODO
+        return null;
+    }
+
+    public void setId(long id) {
+        //TODO
+    }
+
+    public String getUserId() {
+        //TODO
+        return null;
+    }
+
+    public void setUserId(String userId) {
+        //TODO
+    }
+
+    public String getDisplayName() {
+        //TODO
+        return null;
+    }
+
+    public void setDisplayName(String displayName) {
+        //TODO
+    }
+}
