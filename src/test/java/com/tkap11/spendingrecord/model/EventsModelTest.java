4c935c27528ce6a2b2dbd468f90736ec5ca99ffe
Adhytia <adit.14.12.99@gmail.com>
2020-04-30 17:05:43 +0700

    [RED] Create new test

--- src/test/java/com/tkap11/spendingrecord/model/EventsModelTest.java
+++ src/test/java/com/tkap11/spendingrecord/model/EventsModelTest.java
@@ -1,26 +1,23 @@
 package com.tkap11.spendingrecord.model;
 
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EventsModelTest {
 
-    @Mock
+    @InjectMocks
     EventsModel eventsModel;
 
     @Test
     void getEvents() {
-        when(eventsModel.getEvents())
-                .thenReturn(new ArrayList<>());
         assertTrue(eventsModel.getEvents() instanceof List);
     }
 }
