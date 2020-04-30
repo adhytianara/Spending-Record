f7397ec91134168fb698e37c638c401c43843b71
Adhytia <adit.14.12.99@gmail.com>
2020-04-30 17:57:57 +0700

    [GREEN] Implement user table database

--- src/main/java/com/tkap11/spendingrecord/service/DatabaseService.java
+++ src/main/java/com/tkap11/spendingrecord/service/DatabaseService.java
@@ -1,9 +1,35 @@
 package com.tkap11.spendingrecord.service;
 
+
+import com.tkap11.spendingrecord.database.Dao;
+import com.tkap11.spendingrecord.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class DatabaseService {
-    public void registerUser(String userId, String adhytia) {
+
+    @Autowired
+    private Dao mDao;
+
+    public int registerUser(String aUserId, String aDisplayName){
+        if(findUserById(aUserId) == null)
+        {
+            return mDao.registerUser(aUserId, aDisplayName);
+        }
+
+        return -1;
     }
 
-    public void findUserById(String userId) {
+    public String findUserById(String aUserId){
+        List<User> users=mDao.getByUserId("%"+aUserId+"%");
+
+        if(users.size() > 0)
+        {
+            return users.get(0).getUserId();
+        }
+        return null;
     }
 }
