fef415cf2b6ea440fff5278ae447232874d25f9c
Adhytia <adit.14.12.99@gmail.com>
2020-04-30 17:51:57 +0700

    [GREEN] Implement user table database

--- src/main/java/com/tkap11/spendingrecord/database/Config.java
+++ src/main/java/com/tkap11/spendingrecord/database/Config.java
@@ -1,17 +1,21 @@
 package com.tkap11.spendingrecord.database;
 
 import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.LineMessagingClientBuilder;
 import com.linecorp.bot.client.LineSignatureValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
-
-public class Config {
+public class Config
+{
     @Autowired
     private Environment mEnv;
 
@@ -32,10 +36,10 @@ public class Config {
     {
         return LineMessagingClient
                 .builder(getChannelAccessToken())
-                .apiEndPoint("https://api.line.me/")
-                .connectTimeout(10_000)
-                .readTimeout(10_000)
-                .writeTimeout(10_000)
+                .apiEndPoint(LineMessagingClientBuilder.DEFAULT_API_END_POINT)
+                .connectTimeout(LineMessagingClientBuilder.DEFAULT_CONNECT_TIMEOUT)
+                .readTimeout(LineMessagingClientBuilder.DEFAULT_READ_TIMEOUT)
+                .writeTimeout(LineMessagingClientBuilder.DEFAULT_WRITE_TIMEOUT)
                 .build();
     }
 
@@ -43,5 +47,27 @@ public class Config {
     public LineSignatureValidator getSignatureValidator()
     {
         return new LineSignatureValidator(getChannelSecret().getBytes());
+    }
+
+    @Bean
+    DataSource getDataSource()
+    {
+        String dbUrl=System.getenv("JDBC_DATABASE_URL");
+        String username=System.getenv("JDBC_DATABASE_USERNAME");
+        String password=System.getenv("JDBC_DATABASE_PASSWORD");
+
+        DriverManagerDataSource ds=new DriverManagerDataSource();
+        ds.setDriverClassName("org.postgresql.Driver");
+        ds.setUrl(dbUrl);
+        ds.setUsername(username);
+        ds.setPassword(password);
+
+        return ds;
+    }
+
+    @Bean
+    public Dao getPersonDao()
+    {
+        return new DaoImplementation(getDataSource());
     }
 }
--- src/main/java/com/tkap11/spendingrecord/service/DatabaseService.java
+++ src/main/java/com/tkap11/spendingrecord/service/DatabaseService.java
@@ -1,4 +1,9 @@
 package com.tkap11.spendingrecord.service;
 
 public class DatabaseService {
+    public void registerUser(String userId, String adhytia) {
+    }
+
+    public void findUserById(String userId) {
+    }
 }
