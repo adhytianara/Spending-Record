6bb5e6b061c6473cbdd1ee51350a5036ab3135db
Adhytia <adit.14.12.99@gmail.com>
2020-04-30 17:47:17 +0700

    [RED] Create database test

---
+++ src/test/java/com/tkap11/spendingrecord/database/DaoImplementationTest.java
@@ -0,0 +1,20 @@
+package com.tkap11.spendingrecord.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DaoImplementationTest {
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getByUserId() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+}
