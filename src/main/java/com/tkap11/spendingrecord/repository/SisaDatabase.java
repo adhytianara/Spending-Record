8a7f75b48d2278955d687276342c74d2afa0c7e3
ihsan azizi <aziziihsan@gmail.com>
2020-06-22 17:44:53 +0700

    Fix Checkstyle except test(2)

--- src/main/java/com/tkap11/spendingrecord/repository/SisaDatabase.java
+++ src/main/java/com/tkap11/spendingrecord/repository/SisaDatabase.java
@@ -2,22 +2,21 @@ package com.tkap11.spendingrecord.repository;
 
 import com.tkap11.spendingrecord.database.SpendingDao;
 import com.tkap11.spendingrecord.model.Spending;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public class SisaDatabase {
 
-    @Autowired
-    private SpendingDao spendingDao;
+  @Autowired
+  private SpendingDao spendingDao;
 
-    /**
-     * Save user record to database.
-     */
-    public List<Spending> sisaBudget(String description) {
-        String[] userData = description.split(";");
-        return spendingDao.getSisa(userData[0], userData[1]);
-    }
+  /**
+   * Save user record to database.
+   */
+  public List<Spending> sisaBudget(String description) {
+    String[] userData = description.split(";");
+    return spendingDao.getSisa(userData[0], userData[1]);
+  }
 }
